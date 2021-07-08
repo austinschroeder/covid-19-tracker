9612c48c5482ed87bd991b686133f05754a10059
austinschroeder <austinsoundy@gmail.com>
2021-07-08 10:16:34 -0700

    Added cases table

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { useState, useEffect } from 'react';
 import { MenuItem, FormControl, Select, Card, CardContent  } from '@material-ui/core';
 import InfoBox from './components/Infobox'
 import Map from './components/Map'
+import Table from './components/Table'
 import './css/App.css';
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState(["worldwide"]);
   const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -29,7 +31,7 @@ function App() {
             value: country.countryInfo.iso2
           }
         ));
-
+        setTableData(data);
         setCountries(countries);
       });
     };
@@ -80,7 +82,7 @@ function App() {
       <Card className="app-right">
         <CardContent>
           <h3>Live Cases by Country</h3>
-
+          <Table countries={tableData} />
           <h3>Worldwide new cases</h3>
         </CardContent>          
       </Card>
